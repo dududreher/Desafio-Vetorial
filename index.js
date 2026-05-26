fa450b5bf922de31fecc60d159c2eaa90e12b2ba
dududreher <eduardoDreher045@gmail.com>
2026-05-26 14:31:16 -0300

    Update index.js
    adicionado a validacao para impedir numeros negativos e decimais

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 function fatorial(numero) {
 
-    // Verifica se é inteiro e positivo
-    if (!Number.isInteger(numero) || numero < 0) {
-        return "Digite um número inteiro positivo.";
+    
+    if (!Number.isInteger(numero)) {
+        return "O número precisa ser inteiro.";
+    }
+
+    
+    if (numero < 0) {
+        return "Não existe fatorial de número negativo.";
+    }
+
+   
+    if (numero === 0 || numero === 1) {
+        return 1;
     }
 
     let resultado = 1;
 
-    for (let i = 1; i <= numero; i++) {
+    for (let i = 2; i <= numero; i++) {
         resultado *= i;
     }
 
     return resultado;
 }
 
-console.log(fatorial(5));   
-console.log(fatorial(-2));  
-console.log(fatorial(3.5)); 
+console.log(fatorial(5));     
+console.log(fatorial(-3));   
+console.log(fatorial(4.5));   
